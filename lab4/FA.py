c0ccf873eaeb931d759ef5afcb964f1da0f97137
andreea-buiga <baie2879@scs.ubbcluj.ro>
2022-11-13 19:20:57 +0200

    lab4

---
+++ lab4/FA.py
@@ -0,0 +1,79 @@
+class FA:
+    def __init__(self, file_name):
+        self.file = file_name
+        self.FA = self.read_FA()
+        self.FA_Q = self.FA[0]  # set of states
+        self.FA_E = self.FA[1]  # the alphabet
+        self.FA_d = self.represent_transitions(self.FA[4])  # transitions
+        self.FA_q0 = self.FA[2]  # initial state
+        self.FA_F = self.FA[3]  # the final states
+
+    def read_FA(self):
+        fa = []
+        with open(self.file) as f:
+            # get the finite set of states
+            line = f.readline()
+            fa.append(line[0:-1].split(" "))
+            # get the alphabet
+            line = f.readline()
+            fa.append(line[0:-1].split(" "))
+            # get the start state
+            line = f.readline()
+            fa.append(line[0:-1].split(" "))
+            # get the final states
+            line = f.readline()
+            fa.append(line[0:-1].split(" "))
+            # get the transition function
+            d = []
+            line = f.readline()
+            while line:
+                transition = line[0:-1]
+                d.append(transition.split(","))
+                line = f.readline()
+            fa.append(d)
+
+        return fa
+
+    @staticmethod
+    def represent_transitions(transitions):
+        rep = {}
+        for t in transitions:
+            s = (t[0], t[1])
+            if s not in rep.keys():
+                rep[s] = []
+            rep[s].append(t[2])
+        return rep
+
+    def is_deterministic(self):
+        for val in self.FA_d.values():
+            if len(val) > 1:
+                return False
+        return True
+
+    def check_sequence(self, sequence):
+        if not self.is_deterministic():
+            return False
+        current_state = self.get_FA_q0()[0]
+
+        for i in sequence:
+            if (current_state, i) not in self.FA_d.keys():
+                return False
+            current_state = self.FA_d[(current_state, i)][0]
+        if current_state not in self.FA_F:
+            return False
+        return True
+
+    def get_FA_Q(self):
+        return self.FA_Q
+
+    def get_FA_E(self):
+        return self.FA_E
+
+    def get_FA_d(self):
+        return self.FA_d
+
+    def get_FA_q0(self):
+        return self.FA_q0
+
+    def get_FA_F(self):
+        return self.FA_F
---
+++ lab4/main.py
@@ -0,0 +1,49 @@
+from FA import FA
+
+
+def print_menu():
+    print("\n")
+    print("-" * 30)
+    print("choose what you want to display:")
+    print("\t• 1 • the set of states")
+    print("\t• 2 • the alphabet")
+    print("\t• 3 • all transitions")
+    print("\t• 4 • the initial state")
+    print("\t• 5 • the final states")
+    print("\t• 6 • check sequence")
+    print("\t• 0 • to exit")
+    print("-" * 20)
+
+
+def menu():
+    fa = FA("./FA.in")
+    # fa = FA("./FA_identifier.in")
+    while True:
+        try:
+            print_menu()
+            option = int(input("your choice >> "))
+            if option == 1:
+                print(fa.get_FA_Q())
+            elif option == 2:
+                print(fa.get_FA_E())
+            elif option == 3:
+                print(fa.get_FA_d())
+            elif option == 4:
+                print(fa.get_FA_q0())
+            elif option == 5:
+                print(fa.get_FA_F())
+            elif option == 6:
+                seq = input("give sequence to verify >> ")
+                if fa.check_sequence(seq):
+                    print("✔ sequence accepted by FA")
+                else:
+                    print("❌ NOT accepted by FA")
+            elif option == 0:
+                return
+            else:
+                print("wrong option!")
+        except Exception as e:
+            print(e)
+
+
+menu()
